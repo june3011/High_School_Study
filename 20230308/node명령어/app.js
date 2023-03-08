63a0b8930224a48cc9ad3a09ee78c46ed5f5e578
june0623 <junnykee@naver.com>
2023-03-08 11:38:16 +0900

    docker tutorial

---
+++ 20230308/node명령어/app.js
@@ -0,0 +1,7 @@
+var http = require("http");
+var content = function (req, res) {
+  res.end("Welcome to dgsw servr!" + "\n");
+  res.writeHead(200);
+};
+var server = http.createServer(content);
+server.listen(8000);
