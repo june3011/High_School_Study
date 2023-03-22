d23289302a27dd3a820336da03dd2f17bf684d39
june0623 <junnykee@naver.com>
2023-03-22 15:40:43 +0900

    Study : express, mysql connect, docker

---
+++ Docker_Study/20230322/docker명령어.md
@@ -0,0 +1,20 @@
+# docker mysql 실행 명령어
+
+docker run -d --name mymysql -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=word -p 3306:3306 -v d:/mymysql:/var/lib/mysql mysql:5.7
+
+# docker logs mymysql <- docker logs 보는 명령어
+
+##
+
+#
+
+docker exec -it mymysql bash
+
+##
+
+docker run -d --name mynode -p 5000:5000 -v d:/mynode:/app node:18.13.0
+
+##
+
+docker run -it -d --name mynode -p 5000:5000 -v d:/mynode:/app node:18.13.0
+docker exec -it mynode bash
---
+++ Docker_Study/20230322/node_mysql_test/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const pool = mysql.createPool({
+  host: "127.0.0.1",
+  user: "user1",
+  password: "password",
+  database: "word",
+});
+
+const app = express();
+
+app.get("/students", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) throw err;
+
+    connection.query("SELECT * from students", (error, rows, fields) => {
+      if (error) throw error;
+      console.log("User info is : ", rows);
+      res.send(rows);
+    });
+
+    connection.release();
+  });
+});
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+app.listen("5006", () => {
+  console.log("5006listening");
+});
