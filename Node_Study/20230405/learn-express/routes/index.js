87208cec38a6a435fa147f74651390e520d32e53
june0623 <junnykee@naver.com>
2023-04-07 15:20:52 +0900

    Study : router, pug

---
+++ Node_Study/20230405/learn-express/routes/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get((req, res, next) => {
+    res.locals.value = 3;
+    res.locals.array = [
+      { id: 1, title: "a" },
+      { id: 2, title: "b" },
+      { id: 3, title: "c" },
+      { id: 4, title: "d" },
+    ];
+    res.render("index", { name: "USER" });
+  })
+  .post((req, res, next) => {
+    res.send("Hello users");
+  })
+  .put((req, res, next) => {
+    res.send("Hello users");
+  });
+
+module.exports = router;
---
+++ Node_Study/20230405/learn-express/routes/user.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.send("Hello World");
+});
+
+router.get("/:id", (req, res, next) => {
+  const id = req.params.id;
+  const query = req.query;
+  console.log(query);
+  res.send(`Hello ${id}`);
+});
+
+module.exports = router;
