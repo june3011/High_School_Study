d71627a60033bbb3f0a0fb6b13a87520733f5beb
june0623 <junnykee@naver.com>
2023-06-07 22:21:13 +0900

    Study : nodebird, docker-compose, aws, jest

---
+++ Node_Study/20230607/nodebird/controllers/user.test.js
@@ -0,0 +1,33 @@
+jest.mock("../models/user");
+const User = require("../models/user");
+const { describe } = require("../models/user");
+const { follow } = require("./user");
+
+describe("follow", () => {
+  const req = {
+    user: { id: 1 },
+    params: { id: 2 },
+  };
+  const res = {
+    status: jest.fn(() => res),
+    send: jest.fn(),
+  };
+  const next = jest.fn();
+
+  test("사용자를 찾아 팔로잉을 추가하고 success를 응답해야 함", async () => {
+    await follow(req, res, next);
+    expect(res.send).toBeCalledWith("success");
+  });
+
+  test("사용자를 못 찾으면 res.status(404).send(no user)를 호출함", async () => {
+    await follow(req, res, next);
+    expect(res.status).toBeCalledWith(404);
+    expect(res.send).toBeCalledWith("no user");
+  });
+
+  test("DB에서 에러가 발생하면 next(error)를 호출함", async () => {
+    const message = "DB에러";
+    await follow(req, res, next);
+    expect(next).toBeCalledWith(message);
+  });
+});
